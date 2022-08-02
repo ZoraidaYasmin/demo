736c79561b994583156b4cfdcde20a634d03cf20
zoyarce <zoyarce@emeal.nttdata.com>
2022-08-02 16:12:15 -0500

    zoz-first commit

---
+++ ms-buyBootCoin/src/main/java/com/example/demo/BuyBootCoinApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyBootCoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyBootCoinApplication.class, args);
+	}
+
+}
